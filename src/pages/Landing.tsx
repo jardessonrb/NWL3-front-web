071cf165ff98d28b8fdd32028ceee5f27eeaad69
jardesson <jardesson@gmail.com>
2021-01-31 17:33:05 -0300

    front web da aplicação

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {FiArrowRight} from 'react-icons/fi';
+
+import '../styles/pages/landing.css';
+import LogoImg from '../images/Logo.svg';
+
+function Landing() {
+    return (
+        <div id="page-landing">
+        <div className="content-wrapper">
+            <img src={LogoImg} alt="Happy"/>
+
+            <main>
+                <h1>Leve Felicidade para o mundo</h1>
+                <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+
+            </main>
+
+            <div className="location">
+                <strong>Cidade de Altos</strong>
+                <span>Piauí</span>
+            </div>
+
+            <Link to="/app" className="enter-app">
+                <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
+            </Link>
+        </div>
+        {/* <Title text="NLW...."/> */}
+        </div>
+
+    );
+}
+
+
+export default Landing;
